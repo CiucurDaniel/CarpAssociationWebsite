ca110dbc705f718370924f98cf99d53ae8f7835f
Ciucur Daniel <ciucur.daniel14@gmail.com>
2021-05-24 13:55:24 +0300

    Add new ViewModel for Loan and Economy Account

---
+++ CarpAssociationWebsite/ViewModels/ActiveLoansAndEconomyAccountsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CarpAssociationWebsite.Models;
+
+namespace CarpAssociationWebsite.ViewModels
+{
+    public class ActiveLoansAndEconomyAccountsViewModel
+    {
+
+        public List<Loan> Loans { get; set; }
+        public List<EconomyAccount> EconomyAccounts { get; set; }
+
+    }
+}
