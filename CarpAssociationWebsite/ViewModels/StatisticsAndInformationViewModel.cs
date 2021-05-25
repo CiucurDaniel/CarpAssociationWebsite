3b9d61bf453ca4c75919062bdf1df88d90658102
Ciucur Daniel <ciucur.daniel14@gmail.com>
2021-05-26 02:15:45 +0300

    Add StatisticsAndInformationViewModel

---
+++ CarpAssociationWebsite/ViewModels/StatisticsAndInformationViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CarpAssociationWebsite.Models;
+
+namespace CarpAssociationWebsite.ViewModels
+{
+    public class StatisticsAndInformationViewModel
+    {
+        public int ActiveLoans { get; set; }
+        public int ActiveEconomyAccounts { get; set; }
+
+        // public int NewMembersThisMonth { get; set; } i cannot do this because I do not have Date Joined on member
+
+        public int TotalMoneyFromLoanInterestRateThisMonth { get; set; }
+
+        // public int TotalNumberOfMembers { get; set; } we get this from List<Memebers> members
+
+        public List<Member> Members { get; set; }
+    }
+}
