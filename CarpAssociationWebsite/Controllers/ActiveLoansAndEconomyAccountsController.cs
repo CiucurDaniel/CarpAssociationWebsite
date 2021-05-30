1413344bb752960e4cf82c580a52bd28be2f231a
Ciucur Daniel <ciucur.daniel14@gmail.com>
2021-05-30 23:42:08 +0300

    ActiveLoansAndEconomyAccounts Use case implementation

---
+++ CarpAssociationWebsite/Controllers/ActiveLoansAndEconomyAccountsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CarpAssociationWebsite.ViewModels;
+using CarpAssociationWebsite.DataAccessLayer;
+
+namespace CarpAssociationWebsite.Controllers
+{
+    public class ActiveLoansAndEconomyAccountsController : Controller
+    {
+        private readonly CarpAssociationWebsiteContext db = new CarpAssociationWebsiteContext();
+
+        // GET: ActiveLoansAndEconomyAccounts
+        public ActionResult ActiveLoansAndEconomyAccountsIndex()
+        {
+            // get economy accounts
+
+            // TODO: Perform query to get Economy Accounts
+            // var economyAccounts = db.EconomyAccounts.ToList();
+
+            // get loans that are still ongoing  
+
+            var loans = db.Loans.ToList();  // TODO: where status = Ongoing (now we get also the finalized loans)
+
+            ActiveLoansAndEconomyAccountsViewModel loansAndEconomyAccountsViewModel = new ActiveLoansAndEconomyAccountsViewModel()
+            {
+                Loans = loans
+            };
+            return View(loansAndEconomyAccountsViewModel);
+        }
+    }
+}
--- CarpAssociationWebsite/ViewModels/ActiveLoansAndEconomyAccountsViewModel.cs
+++ CarpAssociationWebsite/ViewModels/ActiveLoansAndEconomyAccountsViewModel.cs
@@ -10,7 +10,9 @@ namespace CarpAssociationWebsite.ViewModels
     {
 
         public List<Loan> Loans { get; set; }
-        public List<EconomyAccount> EconomyAccounts { get; set; }
+
+        // TODO: Implement when EconomyAccount is done
+        //public List<EconomyAccount> EconomyAccounts { get; set; }
 
     }
 }
